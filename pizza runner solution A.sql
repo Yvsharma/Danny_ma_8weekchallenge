36223f37b4c78d6489bb49ec87352a5628ee80da
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2023-01-17 17:18:44 +0530

    Add files via upload

---
+++ pizza runner solution A.sql
@@ -0,0 +1,37 @@
+-- A. Pizza Metrics
+select * from customer_orders;
+select * from pizza_names;
+select * from pizza_recipes;
+select * from pizza_toppings;
+select * from runner_orders;
+select * from runners;
+-- 1 How many pizzas were ordered?
+select count(order_id)  as "Total order"from customer_orders;
+
+-- 2 How many unique customer orders were made?
+select count(distinct customer_id) as "Unique cusomers" from customer_orders;
+
+-- 3 How many successful orders were delivered by each runner?
+select count(runner_orders.order_id), runner_id from runner_orders
+where not cancellation like '%Cancellation' or cancellation = 'null'
+group by runner_id;
+
+-- 4 How many of each type of pizza was delivered?
+select pizza_id as 'Pizza ID', count(pizza_id) as 'No. of Pizza' from customer_orders 
+group by pizza_id;
+
+-- 5 How many Vegetarian and Meatlovers were ordered by each customer?
+select customer_id,pizza_id,count(order_id) as 'Total pizza' from customer_orders
+group by customer_id,pizza_id
+order by customer_id asc;
+
+-- 6 What was the maximum number of pizzas delivered in a single order?
+
+-- 7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+-- 8 How many pizzas were delivered that had both exclusions and extras?
+select count(pizza_id) as 'No. of pizza having Exclusions and extras' from customer_orders
+having not exclusions = 'null' and not extras = 'null';
+-- 9 What was the total volume of pizzas ordered for each hour of the day?
+
+-- 10 What was the volume of orders for each day of the week?
