f9cba53bc45ae3f55f2a8588524b7e4495393cba
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2023-01-20 18:19:01 +0530

    Update pizza runner solution A.sql

--- pizza runner solution A.sql
+++ pizza runner solution A.sql
@@ -31,13 +31,49 @@ group by order_id
 order by count(order_id) desc;
 
 -- 7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+solution ;- 7
+SELECT c.customer_id,
+SUM(CASE WHEN (exclusions IS NOT NULL AND exclusions != 0) OR (extras IS NOT NULL AND extras != 0) THEN 1
+ELSE 0 END )AS At_least_One_Change,
+SUM(CASE WHEN (exclusions IS NULL OR exclusions = 0) AND (extras IS NULL OR extras = 0) THEN 1
+ELSE 0 END ) AS No_Change
+FROM customer_orders c
+JOIN runner_orders r
+ON c.order_id = r.order_id
+WHERE r.distance != 0
+GROUP BY c.customer_id;
+
+solution:-8
+SELECT c.customer_id,
+SUM(CASE WHEN (c.exclusions IS NOT NULL AND c.exclusions !=0)
+AND (c.extras IS NOT NULL AND c.extras !=0) THEN 1 ELSE 0 END)AS Both_exclusion_sextra
+FROM customer_orders c
+JOIN runner_orders r
+ON c.order_id = r.order_id
+WHERE r.distance != 0
+GROUP BY c.customer_id
+ORDER BY Both_exclusion_sextra DESC;
+
 
 -- 8 How many pizzas were delivered that had both exclusions and extras?
 select count(order_id) as 'No. of pizza having Exclusions and extras' from customer_orders
 where exclusions not in ('null') and extras not in ('null');
 
 -- 9 What was the total volume of pizzas ordered for each hour of the day?
+solution:- 
+SELECT HOUR(order_time)as hour_time,
+COUNT(order_id)AS Pizzas_Ordered
+FROM customer_orders
+GROUP BY hour_time
+ORDER BY hour_time;
+
 
 -- 10 What was the volume of orders for each day of the week?
 select count(order_id), order_time from customer_orders
 group by order_time
+
+solution:- 
+SELECT dayname(order_time)as day, COUNT(order_id)as Pizzas_ordered
+FROM customer_orders
+GROUP BY day
+ORDER BY Pizzas_ordered DESC;
